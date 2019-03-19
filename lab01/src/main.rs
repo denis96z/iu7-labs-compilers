06ba4ba94a3a3d657bd3e2eb52be32c174d4ac3a
denis96z <zinoviev.denis@yandex.ru>
2019-03-19 20:21:27 +0300

    bug fix

--- lab01/src/main.rs
+++ lab01/src/main.rs
@@ -4,7 +4,7 @@ mod fsm;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
+    if args.len() != 4 {
         eprintln!(
             "USAGE:\n {0} regex dfsm-file-path min-dfsm-file-path",
             &args[0]
@@ -13,5 +13,7 @@ fn main() {
     }
 
     let regex = args[1].clone() + "#";
+    dbg!(&regex);
+
     let dfsm = fsm::DFSM::from_regex(&regex);
 }
