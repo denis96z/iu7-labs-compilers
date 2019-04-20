f145e655511ebae25653ba6afa0bc868765b8cb2
denis96z <zinoviev.denis@yandex.ru>
2019-04-21 01:19:19 +0300

    State and Transition structs added

--- lab01/src/fsm/dfsm.rs
+++ lab01/src/fsm/dfsm.rs
@@ -1,3 +1,28 @@
-pub struct DFSM {}
+use crate::regexp;
+use crate::regexp::vals;
 
-impl DFSM {}
+pub struct DFSM<'a> {
+    states: Vec<State>,
+    valid_values: Vec<State>,
+    transitions: Vec<Transition>,
+    initial_state: &'a State,
+    final_states: &'a [State],
+}
+
+impl<'a> DFSM<'a> {
+    const EMPTY_STATE: State = State(0);
+
+    fn from_regexp(r: &regexp::RegExp) -> Self {
+        unimplemented!()
+    }
+}
+
+#[derive(PartialEq, Clone, Debug)]
+struct State(usize);
+
+#[derive(PartialEq, Clone, Debug)]
+struct Transition {
+    initial_state: State,
+    final_state: State,
+    symbol: vals::Value,
+}
